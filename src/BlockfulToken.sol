afb2c218d8bc4ef415cee558dd0ee22eea3c8e53
lucas picollo <lucaspicolloo@gmail.com>
2024-10-12 10:49:29 -0300

    fix: add initial functions to token

--- src/BlockfulToken.sol
+++ src/BlockfulToken.sol
@@ -7,4 +7,12 @@ contract BlockfulToken {
     function transfer(address to, uint256 amount) public {
         // Implement transfer logic
     }
+
+    function balanceOf(address account) public view returns (uint256) {
+        // Implement balanceOf logic
+    }
+
+    function approve(address spender, uint256 amount) public {
+        // Implement approve logic
+    }
 }
